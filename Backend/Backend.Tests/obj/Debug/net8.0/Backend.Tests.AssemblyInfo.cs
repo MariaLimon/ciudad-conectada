015a85a4ff4c379a9b25a558db4cb8349b1236a8
MariaLimon <limon185mari@gmail.com>
2025-11-03 20:21:26 -0700

    correccion errores de compilacion CI 10

--- Backend/Backend.Tests/obj/Debug/net8.0/Backend.Tests.AssemblyInfo.cs
+++ Backend/Backend.Tests/obj/Debug/net8.0/Backend.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b219be259f8cf70b0a6d2be2323ed2484223be1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14a30e96c8e0db1e9e0cf522829fbdeb58f540d4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
